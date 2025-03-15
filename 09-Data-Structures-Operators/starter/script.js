1b6d32ee51bc2f5587e0acf0e413ee2e3afc992b
Amey <ameygawade11@gmail.com>
2025-03-15 08:39:44 +0530

    string final

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,7 +1,7 @@
 // 'use strict';
 
 // // Data needed for a later exercise
-// const flights =
+// const flight =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // // Data needed for first part of the section
@@ -476,3 +476,43 @@ console.log(challenge('some_variable'));
 console.log(challenge('first_name'));
 console.log(challenge('calculate_age')); 
 console.log(challenge('someVariable'));
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log('textarea'.value);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// =>
+
+console.log(flights.split('+'));
+
+for(const flight of flights.split('+')){
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll('_',' ')} ${from.slice(0,3).toUpperCase()} to ${to.slice(0,3).toUpperCase() } (${time.replace(':', 'h')})`.padStart('40');
+  console.log(output);
+}
+
