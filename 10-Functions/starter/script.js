d13dbd849d0be92d1a5f6c1c4f3670b3fbe838e2
Amey <ameygawade11@gmail.com>
2025-03-16 23:24:59 +0530

    default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightName, numPassenger = 1, price = 2999, totalPrice = 1999 * numPassenger){
+    const booking = {
+        flightName,
+        numPassenger,
+        price,
+        totalPrice
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('AirIndia', 4)
+console.log(bookings); 
+createBooking('AG', undefined, 199)
