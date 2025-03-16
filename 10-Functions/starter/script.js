0b95dababa65282a330c06ccef3c4ac908e8471a
Amey <ameygawade11@gmail.com>
2025-03-16 23:42:13 +0530

    Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -16,3 +16,25 @@ const createBooking = function(flightName, numPassenger = 1, price = 2999, total
 createBooking('AirIndia', 4)
 console.log(bookings); 
 createBooking('AG', undefined, 199)
+
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
